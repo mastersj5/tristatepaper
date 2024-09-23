65cee0ea6435f30aea6f7d755b01f8ed4fb0a21d
Joshua Masters <masters.josh2019@gmail.com>
2024-09-23 00:48:35 -0400

    Revert "Delete client/.storybook directory"
    This reverts commit 4067a1e79be23fa87a243d2a090c2e96c143fd55.

---
+++ client/.storybook/preview.js
@@ -0,0 +1,15 @@
+import { withDesign } from 'storybook-addon-designs';
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [withDesign],
+};
+
+export default preview;
